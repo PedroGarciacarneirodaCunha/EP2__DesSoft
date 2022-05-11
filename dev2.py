2ec181846124e4b42ac12a443af3b802815f7910
Luis Filippe <luisfsg@al.insper.edu.br>
2022-05-11 09:46:38 -0300

    indica o total de tentativas a lista de palpites

--- dev2.py
+++ dev2.py
@@ -19,3 +19,17 @@ print('''
     dica     - Entra no mercado de dicas
     desisto  - Desiste da rodada
 ''')
+
+jogo = True
+
+while jogo:
+    print(
+'''
+Um pais foi escolhido, tente adivinhar!
+Você tem 20 tentativas.
+''')
+
+    tentativas = 20 # tentativas disponíveis
+    palpites = [] # lista com todos os palpites
+
+    
